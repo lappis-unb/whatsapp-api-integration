1ff06a938336e83adfe6b0feea7fdd4cb200bb40
David Carlos <ddavidcarlos1392@gmail.com>
2024-08-10 11:50:45 -0300

    feat: Extract profile name from whatsapp event

--- wpp_event.py
+++ wpp_event.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Any, Dict, Text
+from typing import Any, Dict, List, Text
 import re
 
 from .serpro_api_client import SerproApiClient
@@ -42,6 +42,31 @@ class SerproEvent:
 
 
 @dataclass
+class EventContact:
+    contacts: List
+    name: Text = "Participante"
+    phone: Text = ""
+
+    def __post_init__(self):
+        if self.contacts and len(self.contacts) > 0:
+            self._set_name(self.contacts[0])
+            self._set_phone(self.contacts[0])
+
+    def _set_name(self, contact):
+        if contact:
+            phone_number = contact["wa_id"]
+            if not re.search(r"^(\d{4}9)", phone_number):
+                phone_number = phone_number[:4] + "9" + phone_number[4:]
+            self.phone = f"+{phone_number}"
+
+    def _set_phone(self, contact):
+        if contact:
+            profile = contact.get("profile")
+            if profile:
+                self.name = profile.get("name")
+
+
+@dataclass
 class WhatsAppEvent:
     """
     Generic implementation to manage WhatsApp API events.
@@ -51,6 +76,7 @@ class WhatsAppEvent:
     event_source: SerproEvent | CloudApiEvent | None = None
     parser_class: SerproApiMessagesParser | CloudApiMessagesParser | None = None
     recipient_phone: Text = ""
+    profile_name: Text = "Participante"
     message_types: Dict[str, Any] = field(
         default_factory=lambda: {
             "interactive": InteractiveButtonMessage,
@@ -58,6 +84,7 @@ class WhatsAppEvent:
         }
     )
     sender_id: Text = ""
+    contact: EventContact = field(default_factory=lambda: EventContact([]))
 
     def __post_init__(self):
         if self._event_is_from_cloud_api():
@@ -68,20 +95,20 @@ class WhatsAppEvent:
             self.event_source = SerproEvent(self.event)
             self.parser_class = SerproApiMessagesParser
             self.wpp_client = SerproApiClient()
-        self.set_recipient_phone()
+        self._set_contact()
 
     def get_event_message(
         self,
     ) -> InteractiveButtonMessage | TextMessage | NotSupportedMessage:
-        event_messages = self.get_event_key("messages")
+        event_messages: List = self.get_event_key("messages")
         if event_messages:
-            message = event_messages[0]
+            message: Dict = event_messages[0]
             message_class = self.message_types[message.get("type")]
             return message_class(message)
         return NotSupportedMessage({})
 
     # TODO: check if self.event is a WhatsApp event
-    def get_event_key(self, key: Text) -> Text | None:
+    def get_event_key(self, key: Text):
         return self.event_source.get_event_key(key)
 
     def _event_is_from_cloud_api(self) -> bool:
@@ -93,11 +120,6 @@ class WhatsAppEvent:
             and self.event.get("object") == "whatsapp_business_account"
         )
 
-    def set_recipient_phone(self):
-        contacts = self.get_event_key("contacts")
-        if contacts:
-            contact = contacts[0]
-            phone_number = contact["wa_id"]
-            if not re.search(r"^(\d{4}9)", phone_number):
-                phone_number = phone_number[:4] + "9" + phone_number[4:]
-            self.recipient_phone = f"+{phone_number}"
+    def _set_contact(self):
+        contacts: List = self.get_event_key("contacts")
+        self.contact = EventContact(contacts)
